7e9da4c86bbdacb945b941a5c0c5d7d5e1d89edd
umcondo <superpowerkdg@gmail.com>
2022-07-04 11:21:21 +0900

    Add Home page

--- src/index.js
+++ src/index.js
@@ -2,10 +2,72 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyles = createGlobalStyle`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+  ${"" /* background: ${(prop) => prop.theme.bgColor}; */}
+	line-height: 1;
+  font-family: "Maple_story";
+  overflow-x: hidden;
+}
+h1{
+  font-weight: bold;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+a,ul,li{
+  text-decoration: none;
+  color: inherit;
+}
+`;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <BrowserRouter>
       <App />
     </BrowserRouter>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,70 @@
+import { useNavigate } from "react-router";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MainTextBackground = styled.div`
+  background: url(${process.env.PUBLIC_URL + "assets/home.jpg"});
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: contain;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: white;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 70px;
+  }
+
+  p {
+    font-size: 25px;
+  }
+  p:nth-child(1) {
+    margin-bottom: 50px;
+  }
+  h1 {
+    margin-bottom: 50px;
+    font-size: 70px;
+    text-align: center;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 const Home = () => {
-  return <div>첫페이지</div>;
+  const navigate = useNavigate();
+  const goToLogin = () => {
+    navigate("/login");
+  };
+  return (
+    <MainContainer>
+      <MainTextBackground onClick={goToLogin}>
+        <div>
+          <p>저랑 같이 사실래요?</p>
+          <h1>
+            이웃의
+            <br /> 마켓
+          </h1>
+          <p>이웃과 공동구매</p>
+        </div>
+      </MainTextBackground>
+    </MainContainer>
+  );
 };
 
 export default Home;
