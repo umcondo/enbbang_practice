8bdec8dc8abc594ef3608b9572fbc061776f63d6
umcondo <superpowerkdg@gmail.com>
2022-07-04 10:30:38 +0900

    Add page routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Route, Routes, Navigate } from "react-router";
+import Home from "./pages/Home";
+import Join from "./pages/Join";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>첫페이지</div>;
+};
+
+export default Home;
