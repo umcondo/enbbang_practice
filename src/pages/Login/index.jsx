0a13d6d1cb596e17ff5e1838d26ca53c93b7ffab
umcondo <superpowerkdg@gmail.com>
2022-07-04 11:53:47 +0900

    Add Login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,78 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* flex-direction: column;
+  gap: 20px; */
+
+  button {
+    padding: 15px 30px;
+    border: none;
+    font-size: 20px;
+    border-radius: 10px;
+    background-color: #fee500;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  img:hover {
+    cursor: pointer;
+  }
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 20px;
+  }
+`;
+
 const Login = () => {
-  return <div>카카오로그인</div>;
+  const KAKAO_AUTH_URL =
+    "https://developers.kakao.com/docs/latest/ko/kakaologin/common";
+  return (
+    <MainContainer>
+      <a href={KAKAO_AUTH_URL}>
+        <button>카카오로그인</button>
+
+        <img
+          src={
+            process.env.PUBLIC_URL +
+            "assets/kakao_login/kakao_login_medium_narrow.png"
+          }
+          alt="kakaoLogin"
+        />
+        <img
+          src={
+            process.env.PUBLIC_URL +
+            "assets/kakao_login/kakao_login_medium_wide.png"
+          }
+          alt="kakaoLogin"
+        />
+
+        <img
+          src={
+            process.env.PUBLIC_URL +
+            "assets/kakao_login/kakao_login_large_narrow.png"
+          }
+          alt="kakaoLogin"
+        />
+        <img
+          src={
+            process.env.PUBLIC_URL +
+            "assets/kakao_login/kakao_login_large_wide.png"
+          }
+          alt="kakaoLogin"
+        />
+      </a>
+    </MainContainer>
+  );
 };
 
 export default Login;
